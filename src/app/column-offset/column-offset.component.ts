964d48323eafb1d9a64fb562a55de6d1da770910
Akshay Chavan <akshaychandra.chava@PROD.HCLPNP.COM>
2019-09-18 17:47:03 +0530

    Bootstrap Tutorial

---
+++ src/app/column-offset/column-offset.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-column-offset',
+  templateUrl: './column-offset.component.html',
+  styleUrls: ['./column-offset.component.css']
+})
+export class ColumnOffsetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/nested-rows/nested-rows.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nested-rows',
+  templateUrl: './nested-rows.component.html',
+  styleUrls: ['./nested-rows.component.css']
+})
+export class NestedRowsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
